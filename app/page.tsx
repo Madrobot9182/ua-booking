3a2542b52cc478f88efd2881cbd8575268138004
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2026-02-20 14:01:58 -0700

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@/lib/auth/server";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  // Test logic for logging in 
+  const session = await auth.getSession();
+  if (!session) redirect("/auth/login");
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
