eb3c394c509f01245358d727f448b98797cbd98e
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2026-02-20 12:08:03 -0700

    Fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { authClient } from "@/lib/auth/client";
+import { NeonAuthUIProvider } from "@neondatabase/auth/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { NeonAuthUIProvider } from "@neondatabase/auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
